daf99c5dfb669fce48562497f36ccbc97a972a2d
Arkhopravo Sarkar <91507211+Arkhopravo@users.noreply.github.com>
2025-07-18 07:23:30 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/Authentication/Login'
+import Dashboard from './pages/dashboard/Dashboard'
+import Register from './pages/Authentication/Register'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>
+  }
+])
+
+const App = () => {
+  return (
+    <div className='items-center justify-center flex'>
+       <RouterProvider router= {router}/>
+    </div>
+  )
+}
+
+export default App
